95cd50053a3697e2d1019cfd88d4624bb4bbd964
jackycheung04 <jackycheung04@gmail.com>
2026-09-10 23:57:37 +0800

    Update update_news.py

--- update_news.py
+++ update_news.py
@@ -227,7 +227,7 @@ CATEGORIES_RSS = {
         "https://www.ciodive.com/feeds/news/"
     ],
     "Insights": [
-        "https://review.firstround.com/rss"
+        "https://www.technologyreview.com/feed/"
     ]
 }
 
